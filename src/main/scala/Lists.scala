bb43d8efdd0be8a5bb6de203f35516e76e266a19
Tanjin Panna <tanjin.panna@gmail.com>
2017-11-05 12:40:26 -0500

    P2 - complete, should add tests and docs Also cleaned up P1 a bit

--- src/main/scala/Lists.scala
+++ src/main/scala/Lists.scala
@@ -5,15 +5,34 @@
   */
 object Lists {
 
+  // P1 implemented with Scala built in functions
   def lastBuiltIn[A](list: List[A]): A = {
+    if(list.isEmpty) throw new NoSuchElementException
     list.last
   }
 
+  // P1 using pattern matching on List
   def last[A](list: List[A]): A = {
     list match {
       case Nil => throw new NoSuchElementException  // empty List scenario
-      case single :: Nil => single
-      case _ :: tail => last(tail)
+      case single :: Nil => single                  // single item list scenario
+      case _ :: tail => last(tail)                  // recursion through list to eventually get to last element
+    }
+  }
+
+  // P2 implemented with Scala built in functions
+  def penUltimateBuiltIn[A](list: List[A]): A = {
+    if (list.size < 2) throw new IllegalArgumentException("Input list must contain at least 2 elements")
+    list.takeRight(2).head
+  }
+
+  // P2 using pattern matching on List
+  def penUltimate[A](list: List[A]): A = {
+    if (list.size < 2) throw new IllegalArgumentException("Input list must contain at least 2 elements")
+    list match {
+      case Nil => throw new NoSuchElementException    // empty List scenario
+      case secondToLast :: _ :: Nil => secondToLast   // returns the second to last element
+      case _ :: tail => penUltimate(tail)             // recursion through list to eventually get to penultimate element
     }
   }
 
