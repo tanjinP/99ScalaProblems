3262662d7770d1aa6a4ceaa6efcd2752fe897508
Tanjin Panna <tanjin.panna@gmail.com>
2017-11-05 17:07:38 -0500

    P3 - complete, should add tests and docs

--- src/main/scala/Lists.scala
+++ src/main/scala/Lists.scala
@@ -7,16 +7,16 @@ object Lists {
 
   // P1 implemented with Scala built in functions
   def lastBuiltIn[A](list: List[A]): A = {
-    if(list.isEmpty) throw new NoSuchElementException
+    if (list.isEmpty) throw new NoSuchElementException
     list.last
   }
 
   // P1 using pattern matching on List
   def last[A](list: List[A]): A = {
     list match {
-      case Nil => throw new NoSuchElementException  // empty List scenario
-      case single :: Nil => single                  // single item list scenario
-      case _ :: tail => last(tail)                  // recursion through list to eventually get to last element
+      case Nil => throw new NoSuchElementException // empty List scenario
+      case single :: Nil => single // single item list scenario
+      case _ :: tail => last(tail) // recursion through list to eventually get to last element
     }
   }
 
@@ -30,10 +30,26 @@ object Lists {
   def penUltimate[A](list: List[A]): A = {
     if (list.size < 2) throw new IllegalArgumentException("Input list must contain at least 2 elements")
     list match {
-      case Nil => throw new NoSuchElementException    // empty List scenario
-      case secondToLast :: _ :: Nil => secondToLast   // returns the second to last element
-      case _ :: tail => penUltimate(tail)             // recursion through list to eventually get to penultimate element
+      case Nil => throw new NoSuchElementException // empty List scenario
+      case secondToLast :: _ :: Nil => secondToLast // returns the second to last element
+      case _ :: tail => penUltimate(tail) // recursion through list to eventually get to penultimate element
     }
   }
+
+  // P3 implemented with Scala built in functions
+  def nthBuiltIn[A](index: Int, list: List[A]): A = {
+    if (index < 0) throw new IllegalArgumentException
+    list(index)
+  }
+
+  // P3 using pattern matching on index and List, doing down index until we reach the element of interest
+  def nth[A](index: Int, list: List[A]): A = {
+    (index, list) match {
+      case (0, h :: _) => h
+      case (n, _ :: tail) => nth(n - 1, tail)
+      case (_, Nil) => throw new IllegalArgumentException
+    }
+  }
+
 
 }
