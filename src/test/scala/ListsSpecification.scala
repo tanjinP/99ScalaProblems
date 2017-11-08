87f9249853045ac19d8a0c541fbc8fc877da6d71
Tanjin Panna <tanjin.panna@gmail.com>
2017-11-07 22:40:54 -0500

    P3 - implemented scala check based test lots of conditions - might need to look into this

--- src/test/scala/ListsSpecification.scala
+++ src/test/scala/ListsSpecification.scala
@@ -10,4 +10,8 @@ object ListsSpecification extends Properties("Lists") {
   property("P2: penultimate") = forAll { l: List[Int] =>
     (l.size > 2) ==> (l.takeRight(2).head == Lists.penultimate(l))
   }
+
+  property("P3: nth") = forAll { (i: Int, l: List[Int]) =>
+    (i < l.length && i >= 0 && l.nonEmpty) ==> (l(i) == Lists.nth(i, l))
+  }
 }
