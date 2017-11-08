e805c5bca298f38b93505dffa2856d9fb139031e
Tanjin Panna <tanjin.panna@gmail.com>
2017-11-07 22:34:55 -0500

    P2 - implemented scala check based test might want to look into generic inputs for tests (not just Ints)

--- src/main/scala/Lists.scala
+++ src/main/scala/Lists.scala
@@ -21,18 +21,18 @@ object Lists {
   }
 
   // P2 implemented with Scala built in functions
-  def penUltimateBuiltIn[A](list: List[A]): A = {
+  def penultimateBuiltIn[A](list: List[A]): A = {
     if (list.size < 2) throw new IllegalArgumentException("Input list must contain at least 2 elements")
     list.takeRight(2).head
   }
 
   // P2 using pattern matching on List
-  def penUltimate[A](list: List[A]): A = {
+  def penultimate[A](list: List[A]): A = {
     if (list.size < 2) throw new IllegalArgumentException("Input list must contain at least 2 elements")
     list match {
       case Nil => throw new NoSuchElementException // empty List scenario
       case secondToLast :: _ :: Nil => secondToLast // returns the second to last element
-      case _ :: tail => penUltimate(tail) // recursion through list to eventually get to penultimate element
+      case _ :: tail => penultimate(tail) // recursion through list to eventually get to penultimate element
     }
   }
 
--- src/test/scala/ListsSpecification.scala
+++ src/test/scala/ListsSpecification.scala
@@ -3,7 +3,11 @@ import org.scalacheck.Prop.{forAll, BooleanOperators}
 
 object ListsSpecification extends Properties("Lists") {
 
-  property("last") = forAll { l: List[Int] =>
+  property("P1: last") = forAll { l: List[Int] =>
     l.nonEmpty ==> (l.last == Lists.last(l))
+  }
+
+  property("P2: penultimate") = forAll { l: List[Int] =>
+    (l.size > 2) ==> (l.takeRight(2).head == Lists.penultimate(l))
   }
 }
